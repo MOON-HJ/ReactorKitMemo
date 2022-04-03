1be58d1649dbb1c4c3aa4e48ff969092d124d4e5
Moon Hyo Jae <followmoonrise@gmail.com>
2022-04-03 18:22:58 +0900

    ReactorKit 연습을 위한 Counter View 구성

---
+++ ReactorKitMemo/Counter/CounterViewController.swift
@@ -0,0 +1,75 @@
+//
+//  CounterViewController.swift
+//  ReactorKitMemo
+//
+//  Created by 문효재 on 2022/04/03.
+//
+
+import UIKit
+import SnapKit
+
+class CounterViewController: UIViewController {
+  private let increaseButton: UIButton = {
+    var button = UIButton()
+    button.setTitle("+", for: .normal)
+    button.setTitleColor(UIColor.systemBlue, for: .normal)
+    button.titleLabel?.font = .systemFont(ofSize: 24, weight: .bold)
+    return button
+  }()
+  
+  private let decreaseButton: UIButton = {
+    var button = UIButton()
+    button.setTitle("-", for: .normal)
+    button.setTitleColor(UIColor.systemBlue, for: .normal)
+    button.titleLabel?.font = .systemFont(ofSize: 24, weight: .bold)
+    return button
+  }()
+
+  private let label: UILabel = {
+    var label = UILabel()
+    label.text = "0"
+    label.textColor = .label
+    label.font = .systemFont(ofSize: 30, weight: .heavy)
+    return label
+  }()
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    self.configureUI()
+    self.configureConstraints()
+
+  }
+  
+  init() {
+    super.init(nibName: nil, bundle: nil)
+  }
+    
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  private func configureUI() {
+    self.view.backgroundColor = .systemBackground
+    [increaseButton, label, decreaseButton].forEach {
+      self.view.addSubview($0)
+    }
+  }
+  
+  private func configureConstraints() {
+    decreaseButton.snp.makeConstraints {
+      $0.left.equalToSuperview().offset(24)
+      $0.centerY.equalToSuperview()
+      $0.right.equalTo(label).offset(-24)
+    }
+    
+    label.snp.makeConstraints {
+      $0.center.equalToSuperview()
+    }
+    
+    increaseButton.snp.makeConstraints {
+      $0.left.equalTo(label).offset(24)
+      $0.centerY.equalToSuperview()
+      $0.right.equalToSuperview().offset(-24)
+    }
+  }
+}
