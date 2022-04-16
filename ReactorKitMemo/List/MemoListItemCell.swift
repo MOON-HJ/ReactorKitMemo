1327cc69308a1d21a4adf685f59956fe95c2fb42
Moon Hyo Jae <followmoonrise@gmail.com>
2022-04-16 23:55:29 +0900

    feat : MemoListItemCell 레이블 추가

--- ReactorKitMemo/List/MemoListItemCell.swift
+++ ReactorKitMemo/List/MemoListItemCell.swift
@@ -8,34 +8,44 @@
 import UIKit
 import SnapKit
 
-class MemoListItemCell: UITableViewCell {
+class MemoListItemCell: BaseTableViewCell {
   static let id: String = "MemoListItemCell"
   private var item: MemoListItem?
   
-  private let titleLabel = UILabel()
-
+  private let titleLabel = UILabel().then {
+    $0.backgroundColor = .systemGreen
+    $0.textColor = .black
+    $0.font = .systemFont(ofSize: 16)
+    $0.sizeToFit()
+  }
+  
   override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
     super.init(style: style, reuseIdentifier: reuseIdentifier)
-    configureUI()
-    configureConstraints()
   }
   
   required init?(coder: NSCoder) {
     fatalError("init(coder:) has not been implemented")
   }
+  
 
   public func configure(item: MemoListItem) {
+    super.configure()
+    
     self.item = item
+    self.titleLabel.text = item.title
   }
   
-  private func configureUI() {
-    self.contentView.backgroundColor = .white
+  override func configureUI() {
+    super.configureUI()
+    contentView.backgroundColor = .lightGray
+    
     [titleLabel].forEach {
       contentView.addSubview($0)
     }
   }
   
-  private func configureConstraints() {
+  override func setupConstraints() {
+    super.setupConstraints()
     titleLabel.snp.makeConstraints {
       $0.edges.equalToSuperview()
     }
